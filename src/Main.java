eca4223fc69d46321c05b7cffff8403358bea35c
Aruuke Kenzhalieva <aruukekenzhaliyeva@gmail.com>
2026-02-03 22:50:46 +0300

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import com.sun.jdi.IntegerValue;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("The broken sword");
+        list1.add("Sleeping Beauty");
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        //start
+    }
+}
